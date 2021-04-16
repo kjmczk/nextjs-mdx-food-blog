54a6aafd6f3418a9418f5cf1dc206eb98b9d4788
kjmczk <vollgong@gmail.com>
2021-04-16 15:41:54 +0900

    Use Meta component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,15 +1,21 @@
 import Header from '../components/Header';
+import Meta from '../components/Meta';
 
 type Props = {
   children: React.ReactNode;
+  pageTitle?: string;
 };
 
-const Layout: React.FC<Props> = ({ children }: Props) => {
+const Layout: React.FC<Props> = ({ children, pageTitle }: Props) => {
   return (
-    <div className="max-w-prose mx-auto px-4">
-      <Header />
-      <main className="mt-4">{children}</main>
-    </div>
+    <>
+      <Meta pageTitle={pageTitle} />
+
+      <div className="max-w-prose mx-auto px-4">
+        <Header />
+        <main className="pt-4 pb-12">{children}</main>
+      </div>
+    </>
   );
 };
 
