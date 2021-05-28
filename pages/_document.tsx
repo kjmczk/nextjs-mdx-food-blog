d4c4566e6363845f3b7b5a8071b6ea2ffdbe0b75
kjmczk <vollgong@gmail.com>
2021-05-28 17:59:58 +0900

    Add pages/_document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,17 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render(): React.ReactElement {
+    return (
+      <Html>
+        <Head />
+        <body className="bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
