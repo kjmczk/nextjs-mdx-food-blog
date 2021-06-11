06e209a2b8454956855a451e00400daafce3226f
kjmczk <vollgong@gmail.com>
2021-06-11 19:18:14 +0900

    Use MdxComponentsProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import type { AppProps } from 'next/app';
 
+import { MdxComponentsProvider } from '../context/MdxComponents';
+
 import 'tailwindcss/tailwind.css';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <MdxComponentsProvider>
+      <Component {...pageProps} />;
+    </MdxComponentsProvider>
+  );
 };
 
 export default MyApp;
